17222ff762c7d58023d104796980ed899a1c48e3
Dasari Varun Teja <dvarunteja908@gmail.com>
2026-04-24 15:13:19 +0530

    meditrack fifth commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,   //  VERY IMPORTANT
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
 const promisePool = pool.promise();
