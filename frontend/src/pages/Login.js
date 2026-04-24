8c21daabd90dfcf94b27cbb951cbbfd6dc1b818a
Dasari Varun Teja <dvarunteja908@gmail.com>
2026-04-24 14:32:46 +0530

    meditrack third commit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ export default function Login() {
     setError('');
     setLoading(true);
     try {
-      const res = await fetch('http://127.0.0.1:5000/api/auth/login', {
+      const res = await fetch('https://pharmadesk-ijjj.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
